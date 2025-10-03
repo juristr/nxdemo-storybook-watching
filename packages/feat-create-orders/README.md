c298f14b8c9217d81e84989e1f1152811bd4c46f
Juri <juri.strumpflohner@gmail.com>
2025-10-03 21:34:04 +0200

    add storybook

---
+++ packages/feat-create-orders/README.md
@@ -0,0 +1,7 @@
+# @storybooknx/feat-create-orders
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @storybooknx/feat-create-orders` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/feat-create-orders/src/lib/feat-create-orders.tsx
@@ -0,0 +1,13 @@
+import styles from './feat-create-orders.module.css';
+import { Button } from '@storybooknx/ui';
+
+export function StorybooknxFeatCreateOrders() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to StorybooknxFeatCreateOrders!</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default StorybooknxFeatCreateOrders;
