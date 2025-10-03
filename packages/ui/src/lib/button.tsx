d457049f0b07c40cd20af0582eebf51d7547c2d4
Juri <juri.strumpflohner@gmail.com>
2025-10-03 21:23:14 +0200

    ui lib

---
+++ packages/ui/src/lib/button.tsx
@@ -0,0 +1,24 @@
+import styles from './button.module.css';
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary';
+}
+
+export function Button({
+  variant = 'primary',
+  children,
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={`${styles['button']} ${styles[variant]} ${className || ''}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
---
+++ packages/ui/src/lib/ui.tsx
@@ -0,0 +1,11 @@
+import styles from './ui.module.css';
+
+export function StorybooknxUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to StorybooknxUi!</h1>
+    </div>
+  );
+}
+
+export default StorybooknxUi;
